0f62cb76f7b582a89b4fb22f23cd265ba1d4495a
Darshan H <darshan8983@gmail.com>
2022-09-16 12:14:56 +0530

    stage 02 convert to lowercase str

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -3,7 +3,8 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories, get_df
+import numpy as np
 from src.utils.data_mgmt import process_posts
 import random
 
@@ -22,7 +23,7 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    
+
     artifacts = config["artifacts"]
     prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
     train_data_path = os.path.join(prepared_data_dir_path, artifacts["TRAIN_DATA"])
@@ -34,6 +35,15 @@ def main(config_path, params_path):
     featurized_train_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TRAIN"])
     featurized_test_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TEST"])
 
+    max_features = params["featurize"]["max_features"]
+    ngrams = params["featurize"]["ngrams"]
+
+    df_train = get_df(train_data_path)
+
+    train_words = np.array(df_train.text.str.lower().values.astype("U"))
+
+    print(train_words[:20])
+    
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
